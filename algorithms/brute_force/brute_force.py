Fixed brute force calculation / added some output of progress

algorithms/brute_force/brute_force.py
@@ -18,15 +18,18 @@ class BruteForceRunner(AlgorithmRunner):
 
     def format_bounds_brute_force(self):
         return [(self.bounds[0][i], self.bounds[1][i])for i in range(len(self.bounds[0]))]
+
     def get_max_param_range(self):
-        return max([float(self.bounds[1][i] - self.bounds[0][i]) for i in range(len(self.bounds[0]))])
+        return max([float(self.bounds[1][i] - self.bounds[0][i] + 1) for i in range(len(self.bounds[0]))])
+
     def calc_num_permutations(self):
-        return int(np.prod([float(self.bounds[1][i] - self.bounds[0][i]) for i in range(len(self.bounds[0]))]))
+        return int(np.prod([float(self.bounds[1][i] - self.bounds[0][i] + 1) for i in range(len(self.bounds[0]))]))
+    
     def suggest(self):
         if self.calc_num_permutations() > 1e+6:
-            logger.error(f"Number of Permuations >{self.calc_num_permutations()}< is too large (max: 1e+6). going to exit.")
+            logger.error(f"Number of permuations >{self.calc_num_permutations()}< is too large (max: 1e+6). going to exit.")
             sys.exit()
-        logger.warning(f"Going to generate Permuations {self.calc_num_permutations()} of {len(self.bounds[0])} Parameters (max range: {self.get_max_param_range()}). There will be no log while generating. So dont get stressed :).")
+        logger.warning(f"Going to generate permuations {self.calc_num_permutations()} of {len(self.bounds[0])} parameters (max range: {self.get_max_param_range()}). There will be no log while generating. So dont get stressed :).")
         xx = product(*[list(range(b[0],b[1]+1)) for b in self.format_bounds_brute_force()])
         xx = tensor([x for x in xx])
         return xx

use_cases/mrp/mrp_runner.py
@@ -41,7 +41,6 @@ class MRPRunner():
 
 
     def eval(self, x, ):
-
         x = self.transform_x(x)
         self.X.append(x)
         releases = self.run_solver(x)
@@ -51,18 +50,20 @@ class MRPRunner():
         y = self.get_mean_and_sem_from_y(self.Y_raw[-self.num_solver_runs:])
   
         return y
+
     def get_mean_and_sem_from_y(self, y_raw):
         data = list()
 
         for i in range(len(y_raw[0].keys())):
             values = [list(y.values())[i] for y in y_raw]
             mean = np.mean(values)
-            sem = (np.std(values, ddof=1)/np.sqrt(np.size(values)))
+            sem = (np.std(values, ddof=1) / np.sqrt(np.size(values)))
             data.append((mean,sem))
         return data
 
     def format_y_for_candidate(self, y):
         return {"costs" : y[0], "service_level" : y[1]}
+
     def format_x_for_candidate(self, x):
         return self.transform_x(x)
 