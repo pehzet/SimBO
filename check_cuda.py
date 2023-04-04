9cc059f6af3f47d50cb5a3ee3997a9521315053e
pehzet <philipp.zmijewski@hs-osnabrueck.de>
2023-04-04 11:59:40 +0200

    torch check

--- check_cuda.py
+++ check_cuda.py
@@ -1,4 +1,5 @@
 import torch
 print(f"CUDA installed? {torch.cuda.is_available()}")
-
+available_gpus = [torch.cuda.device(i) for i in range(torch.cuda.device_count())]
+print(f"Available GPUs: {available_gpus}")
 torch.Tensor([1, 2, 3]).to("cuda")
